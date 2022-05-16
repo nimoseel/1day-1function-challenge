039d043caccb9aa0b248cd3a8e62c0f49f67bedf
somin Lee <85738589+nimoseel@users.noreply.github.com>
2022-05-16 22:47:34 +0900

    Delete 윤년.js

--- 윤년.js
+++
@@ -1,7 +0,0 @@
-function isLeapYear(year){
-    return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)
-}
-
-console.log(isLeapYear(2020))
-console.log(isLeapYear(2010))
-console.log(isLeapYear(2000))
