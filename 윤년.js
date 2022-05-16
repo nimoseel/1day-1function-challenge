3023a325b7867921f7355b9b93a801c3cdf53c56
“nimoseel” <fromsomini@gmail.com>
2022-05-16 22:34:23 +0900

    윤년 구하기

---
+++ 윤년.js
@@ -0,0 +1,7 @@
+function isLeapYear(year){
+    return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)
+}
+
+console.log(isLeapYear(2020))
+console.log(isLeapYear(2010))
+console.log(isLeapYear(2000))
