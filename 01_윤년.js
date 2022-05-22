f5c373a0e39db4db74870a4bab33db4e4c2e512f
“nimoseel” <fromsomini@gmail.com>
2022-05-22 22:12:14 +0900

    윤년 함수 추가

--- 01_윤년.js
+++ 01_윤년.js
@@ -5,3 +5,12 @@ function isLeapYear(year){
 console.log(isLeapYear(2020))
 console.log(isLeapYear(2010))
 console.log(isLeapYear(2000))
+
+
+// 기본 매개변수를 추가한 윤년 함수
+function isLeapYear1(year=new Date().getFullYear()){
+    console.log(`매개변수 year: ${year}`)
+    return (year % 4 === 0 ) && (year % 100 !== 0) || (year % 400 === 0)
+}
+
+console.log(isLeapYear1(2020))
