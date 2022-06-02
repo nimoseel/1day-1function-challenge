42b8b3befc0817b18efea4792f520f1d00f3c720
“nimoseel” <fromsomini@gmail.com>
2022-06-03 06:11:36 +0900

    0604 챌린지

---
+++ 14_소수판별.js
@@ -0,0 +1,15 @@
+function checkPrime(n) {
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+        console.log("NO");
+        return false;
+        }
+    }
+    if (n === 1) {
+        console.log("NO");
+        return false;
+    }
+    console.log("YES");
+}
+
+checkPrime(n);
