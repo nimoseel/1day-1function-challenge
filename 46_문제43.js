4c591aa5e41c042c5375109a55997fb827c56475
“nimoseel” <fromsomini@gmail.com>
2022-09-14 10:33:52 +0900

    0914 챌린지

---
+++ 46_문제43.js
@@ -0,0 +1,14 @@
+// 사용자에게 숫자를 입력받고 이를 2진수를 바꾸고 그 값을 출력해주세요.
+let dec = prompt();
+
+console.log(parseInt(dec).toString(2));
+
+// let dec = prompt();
+// let arrBinary = [];
+
+// while(dec){
+//     arrBinary.push(dec%2);
+//     dec = Math.floor(dec/2);
+// }
+
+// console.log(arrBinary.reverse().join(""));
