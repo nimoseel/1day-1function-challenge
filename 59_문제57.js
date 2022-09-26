cd08e106c06bc4160821dc78182adeedcff30bcd
“nimoseel” <fromsomini@gmail.com>
2022-09-26 11:04:05 +0900

    0926 챌린지

---
+++ 59_문제57.js
@@ -0,0 +1,18 @@
+// 0부터 1000까지 1의 개수를 세는 프로그램
+// 0부터 1000까지의 수에서 1은 몇 번이나 들어갔을까요? 출력해 주세요.
+const obj = {};
+
+for (let i = 0; i <= 1000; i++) {
+    let tmp = i;
+    while (tmp > 0) {
+        let num = tmp % 10;
+        if (obj[num]) {
+            obj[num]++;
+        } else {
+            obj[num] = 1;
+        }
+        tmp = parseInt(tmp/10, 10);
+    }
+}
+
+console.log(obj[1]);
