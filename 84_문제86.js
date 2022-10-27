f066dbf7dd70e6f99a2ca83fc933187f721886a2
“nimoseel” <fromsomini@gmail.com>
2022-10-27 10:49:29 +0900

    1027 챌린지

---
+++ 84_문제86.js
@@ -0,0 +1,36 @@
+const point = [1,1,3,2,5];
+const dish = 3;
+
+function solution(point, dish){
+    let answer = 0;
+    dish -= 1;
+
+    let s = point.slice();
+
+    s.sort((a,b)=>{
+        return a-b;
+    })
+
+    while(true){
+        let p = point.shift();
+
+        if(s[0] === p){
+            if(dish ===0){
+                break;
+            }
+            dish -= 1;
+            s.shift();
+        } else {
+            point.push(p);
+            if(dish === 0){
+                dish = point.length - 1;
+            } else {
+                dish -= 1;
+            }
+        }
+        answer += 1;
+    }
+    return answer;
+}
+
+console.log(solution(point, dish));
