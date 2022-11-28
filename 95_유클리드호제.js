decc9178680d4b6b6e910e8b823af79538755602
“nimoseel” <fromsomini@gmail.com>
2022-11-28 13:31:03 +0900

    1128 챌린지

--- 95_유클리드호제.js
+++ 95_유클리드호제.js
@@ -6,7 +6,10 @@ function solution(denum1, num1, denum2, num2) {
     let num = num1 * num2;
     let denum = (denum1 * num2) + (denum2 * num1) ;
     
-    const getGcd = (a, b) => (b === 0 ? a : getGcd(b, a % b));
+    const getGcd = (a, b) => (a % b === 0 ? b : getGcd(b, a % b));
+    // 큰 수를 작은 수로 나눈 나머지가 0이면 작은 수가 최대공약수
+    // 나머지가 0이 아니라면 작은 수를 다시 나머지로 나눔
+    // 나머지가 0이 될 때의 수가 최대공약수
     const gcd = getGcd(denum, num);
 
     return [denum/gcd, num/gcd];
