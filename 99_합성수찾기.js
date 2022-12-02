698187e0e8d27b6b10dbed346290dfdf4bec8f92
“nimoseel” <fromsomini@gmail.com>
2022-12-02 14:27:21 +0900

    1202 챌린지

--- 99_합성수찾기.js
+++ 99_합성수찾기.js
@@ -1,4 +1,5 @@
-// 약수의 개수가 세 개 이상인 수를 합성수라고 합니다. 자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수를 return하도록 solution 함수를 완성해주세요.
+// 약수의 개수가 세 개 이상인 수 - 합성수
+// 자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수 return
 
 function solution(n) {
     let answer = new Set();
