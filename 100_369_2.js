9d94cc5e9bc69975f888e7b918b32b8afeefccce
“nimoseel” <fromsomini@gmail.com>
2022-12-05 09:46:09 +0900

    1205 챌린지

---
+++ 100_369_2.js
@@ -0,0 +1,10 @@
+// 숫자 order가 매개변수로 주어질 때, 쳐야할 박수 횟수를 return
+
+function solution(order) {
+    return Array.from(order.toString()).filter(t => ['3', '6', '9'].includes(t)).length;
+}
+
+function solution2(order) {
+    var answer = [...order.toString().matchAll(/[3|6|9]/g)].length;
+    return answer;
+}
